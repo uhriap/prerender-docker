edaef967a78e384302349430dbf8fa1502281fd5
Rogier Slag <rogier.slag@gmail.com>
2016-06-09 17:52:29 +0200

    Forward headers sent to prerender

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 var prerender = require('prerender');
 
+var forwardHeaders = require('./forwardHeaders');
+var clearPhantomCache = require('./clearPhantomCache');
+
 var server = prerender({
-	workers: process.env.PRERENDER_NUM_WORKERS || 4
+  workers: process.env.PRERENDER_NUM_WORKERS || 4
 });
 
-
-server.use({
-	//There are some issues with the in-memory cache from PhantomJS 2. It keeps pages in memory,
-	//and then simply returns a 304 instead of a 200 whenever possible.
-	//Prerender unfortunatally simply proxies this status code, even though the client doesn't necessarily
-	//have the page in cache.
-	//In order to prevent this we need to clear the memory cache of PhantomJS before each request
-	onPhantomPageCreate: function(phantom, req, res, next) {
-		//The run function is exposed by phridge (bridge between prerender and PhantomJS), and allows
-		//us to run commands in the page context
-		req.prerender.page.run(function(resolve) {
-			this.clearMemoryCache();
-			resolve();
-		})
-		.then(function() { next() })
-		.catch(function() { next() });
-	}
-});
+server.use(clearPhantomCache);
+server.use(forwardHeaders);
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
 server.start();
 
+function shutdown() {
+  console.log('Shutdown initiated');
+  server.exit();
+  // At this point prerender has started killing its phantom workers already.
+  // We give it 5 seconds to quickly do so, and then halt the process. This
+  // will ensure relatively rapid redeploys (prerender no longer accepts new
+  // requests at this point
+  setTimeout(function () {
+    console.log('Prerender has shut down');
+    process.exit();
+  }, 5000);
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
