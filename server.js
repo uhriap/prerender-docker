859d4e07cc481d61f308f062a1f004d117055df8
Rogier Slag <rogier.slag@gmail.com>
2016-11-25 12:15:15 +0100

    Switch to ES6, apply some additional improvements

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var prerender = require('prerender');
+const prerender = require('prerender');
 
-var forwardHeaders = require('./forwardHeaders');
-var stripHtml = require('./stripHtml');
+const forwardHeaders = require('./forwardHeaders');
+const stripHtml = require('./stripHtml');
 
-var server = prerender({
-  workers: process.env.PRERENDER_NUM_WORKERS || 4,
-  iterations: process.env.PRERENDER_NUM_ITERATIONS || 25,
-  softIterations: process.env.PRERENDER_NUM_SOFT_ITERATIONS || 10
-});
+const options = {
+	workers : process.env.PRERENDER_NUM_WORKERS || 4,
+	iterations : process.env.PRERENDER_NUM_ITERATIONS || 25,
+	softIterations : process.env.PRERENDER_NUM_SOFT_ITERATIONS || 10,
+	jsTimeout : process.env.JS_TIMEOUT || 30000,
+	jsCheckTimeout : 600,
+};
+console.log('Starting with options:', options);
+
+const server = prerender(options);
 
 server.use(forwardHeaders);
 server.use(prerender.sendPrerenderHeader());
@@ -17,18 +22,18 @@ server.use(stripHtml);
 
 server.start();
 
-function shutdown() {
-  console.log('Shutdown initiated');
-  server.exit();
-  // At this point prerender has started killing its phantom workers already.
-  // We give it 5 seconds to quickly do so, and then halt the process. This
-  // will ensure relatively rapid redeploys (prerender no longer accepts new
-  // requests at this point
-  setTimeout(function () {
-    console.log('Prerender has shut down');
-    process.exit();
-  }, 5000);
-}
+const shutdown = () => {
+	console.log('Shutdown initiated');
+	server.exit();
+	// At this point prerender has started killing its phantom workers already.
+	// We give it 5 seconds to quickly do so, and then halt the process. This
+	// will ensure relatively rapid redeploys (prerender no longer accepts new
+	// requests at this point
+	setTimeout(() => {
+		console.log('Prerender has shut down');
+		process.exit();
+	}, 5000);
+};
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
